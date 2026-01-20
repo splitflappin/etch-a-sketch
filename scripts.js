e97e6b3e842fdc76b4dadd0a62d105a4041497cf
Split Flappin <splitflappin@gmail.com>
2026-01-19 22:40:28 -0500

    wired of the grid using flex box
    added conatiner to html and used a for loop to create 16 squares.
    Created class square to have a border in addition to have it so when
    it is at max width they perfect squares.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  const divEl = document.createElement("div");
+  divEl.classList.add("square");
+  container.appendChild(divEl);
+}
